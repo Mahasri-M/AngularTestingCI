f8edc76a8ef7d616a8e700255a80e89fece7b145
Mahasri-M <127306995+Mahasri-M@users.noreply.github.com>
2023-10-20 16:52:23 +0530

    test

--- AngularTesting/src/app/app.component.spec.ts
+++ AngularTesting/src/app/app.component.spec.ts
@@ -1,29 +1,36 @@
-import { TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { SampleComponent } from './sample/sample.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
-  }));
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [AppComponent, SampleComponent],
+      imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
+    }).compileComponents();
+  });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
+    fixture.detectChanges(); 
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'AngularTesting'`, () => {
+  it(`should have as title 'angularjest'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('AngularTesting');
+    fixture.detectChanges(); 
+    expect(app.title).toEqual('angularjest');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
+    fixture.detectChanges(); 
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('AngularTesting app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angularjest app is running!');
   });
 });
